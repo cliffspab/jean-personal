6388eb67b0d8998300433131f4df3e19e5db0c26
Sean McPherson <git@seanmcp.com>
2024-09-24 09:30:21 -0400

    update styles

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ window.engines
     row.dataset.shortcut = shortcut;
     row.innerHTML = `<td>${label}</td>
         <td>${shortcut}</td>
-        <td>${url.includes("%s") ? "✔️" : ""}</td>`;
+        <td>${url.includes("%s") ? `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 256"><path d="M232.49,80.49l-128,128a12,12,0,0,1-17,0l-56-56a12,12,0,1,1,17-17L96,183,215.51,63.51a12,12,0,0,1,17,17Z"></path></svg>` : ""}</td>`;
     rows.push(row);
     lookup[shortcut] = {
       element: row,
