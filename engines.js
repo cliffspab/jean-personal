c9ebed05470b23a0f7008a01b05ff548d7b21a34
cliffspab <75878433+cliffspab@users.noreply.github.com>
2024-12-27 02:18:33 +0700

    Update engines.js

--- engines.js
+++ engines.js
@@ -3,25 +3,20 @@
  * LABEL,SHORTCUT,URL (with %s for search term)
  */
 window.engines = `
-Software Cracked,zz,https://cse.google.com/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&gsc.sort=
-GitHub,gh,https://google.com/search?q=site:github.com
-Greasyfork,gf,https://google.com/search?q=site:greasyfork.org
-Reddit,r,https://google.com/search?q=site:reddit.com
-Play Store,pl,https://play.google.com/store/search?q=
+Cracked,zz,https://cse.google.com/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&gsc.sort=
+GitHub,gh,https://google.com/search?q=site:github.com+%s
+Greasyfork,gf,https://google.com/search?q=site:greasyfork.org+%s
+Reddit,r,https://google.com/search?q=site:reddit.com+%s
+PlayStore,pl,https://play.google.com/store/search?q=%s
 brave,b,https://search.brave.com/search?q=%s
 facebook,fb,https://www.facebook.com/search/top/?q=%s&opensearch=1
-Firefox Add-ons
-,,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=%s
-,,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&udm=2&q=%s
-Firefox Add-ons,fa,https://www.google.com/search?q=site:addons.mozilla.org+%s
-Bangkok Post,bkp,https://www.google.com/search?q=site:bangkokpost.com+%s
+fox,fa,https://addons.mozilla.org/en-US/firefox/search/?q=%s
+BangkokPost,bkp,https://www.google.com/search?q=site:bangkokpost.com+%s
 Chocolatey,choco,https://www.google.com/search?q=site:chocolatey.org/packages+%s
 Malwaretips,mal,https://www.google.com/search?q=site:malwaretips.com+%s
 softpedia,s,https://www.google.com/search?q=site:softpedia.com+%s
 faces,f,https://www.google.com/search?tbm=isch&q=%s&ref=%opensearch&imgtype=face
-youtube,yt,https://www.youtube.com/results?search_query=%s&page==%s
-porn,p,
-https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=%s site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+anal+-ladyboy+-shemale+-gay
-p2,a,http://google.com/search?q=%s anal site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+-ladyboy+-shemale+-gay
-
+youtube,yt,https://www.youtube.com/results?search_query=%s
+pron,p,https://www.google.com/search?q=%s&site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+anal+-ladyboy+-shemale+-gay
+p2,a,http://google.com/search?q=%s+anal+site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com&+-ladyboy+-shemale+-gay
 `;
