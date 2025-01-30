96794a312f999b8d19c61de65574dcfcfb84ccc6
cliffspab <75878433+cliffspab@users.noreply.github.com>
2025-01-30 22:04:22 +0700

    Update engines.js
    test

--- engines.js
+++ engines.js
@@ -3,6 +3,7 @@
  * LABEL,SHORTCUT,URL (with %s for search term)
  */
 window.engines = `
+Google,,https://google.com/search?hl=en&safe=off&num=50&pws=0&q=%s
 Cracked,zz,https://cse.google.com/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&gsc.sort=
 GitHub,gh,https://google.com/search?q=site:github.com+%s
 Greasyfork,gf,https://google.com/search?q=site:greasyfork.org+%s
