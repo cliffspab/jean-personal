18b297fbc65fd1fc4a3ce9b5c5222640955533b3
cliffspab <75878433+cliffspab@users.noreply.github.com>
2025-09-10 16:00:03 +0700

    Update engines.js
    small changes

--- engines.js
+++ engines.js
@@ -3,27 +3,26 @@
  * LABEL,SHORTCUT,URL (with %s for search term)
  */
 window.engines = `
-Google,g,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=%s
-Cracked,zz,https://cse.google.com/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&gsc.sort=
-GitHub,gh,https://google.com/search?q=site:github.com+%s
-Greasyfork,gf,https://google.com/search?q=site:greasyfork.org+%s
-Reddit,r,https://google.com/search?q=site:reddit.com+%s
-PlayStore,pl,https://play.google.com/store/search?q=%s
-brave,b,https://search.brave.com/search?q=%s
-facebook,fb,https://www.facebook.com/search/top/?q=%s&opensearch=1
-firefox-addons,fa,https://addons.mozilla.org/en-US/firefox/search/?q=%s
-BangkokPost,bkp,https://www.google.com/search?q=site:bangkokpost.com+%s
-Chocolatey,choco,https://www.google.com/search?q=site:chocolatey.org/packages+%s
-Malwaretips,mal,https://www.google.com/search?q=site:malwaretips.com+%s
-softpedia,s,https://www.google.com/search?q=site:softpedia.com+%s
-faces,f,https://www.google.com/search?tbm=isch&q=%s&ref=%opensearch&imgtype=face
-youtube,yt,https://www.youtube.com/results?search_query=%s
-chromeextensions,e,https://www.google.com/search?gl=us&hl=en&safe=off&num=50&pws=0&q=site:chromewebstore.google.com+%s
-G2A,g2a,https://www.g2a.com/search?query=%s
-porna2,a2,https://www.google.com/search?hl=en&gl=us&safe=off&num=50&pws=0&udm=2&q=%s +porn|fuck|video|hd +-ladyboy+-shemale+-gay++intext:ass+intext:butt+intext:x+intext:anal
-edgeaddons,ea,https://www.google.com/search?q=site:microsoftedge.microsoft.com/addons/search+%s
-porn,a,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&udm=2&q=%s%20site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:eporner.com%20OR%20site:sxyprn.com%20OR%20site:thethothub.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+anal%20+-ladyboy+-shemale+-gay
-porn2,p,http://google.com/search?q=%s+anal+site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+%s+-ladyboy+-shemale+-gay
-cloudsearch,c,https://cloudsearch.google.com/cloudsearch/search?authuser=0&q=%s&it=1609926023312&iq=false
+Google,g,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=%s&ie=UTF-8
+Cracked,zz,https://cse.google.com/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&ie=UTF-8&gsc.sort=
+GitHub,gh,https://google.com/search?hl=en&safe=off&num=50&pws=0&q=site:github.com+%s&ie=UTF-8
+Greasyfork,gf,https://google.com/search?hl=en&safe=off&num=50&pws=0&q=site:greasyfork.org+%s&ie=UTF-8
+Reddit,r,https://google.com/search?hl=en&safe=off&num=50&pws=0&q=site:reddit.com+%s&ie=UTF-8
+PlayStore,pl,https://play.google.com/store/search?q=%s&ie=UTF-8
+brave,b,https://search.brave.com/search?q=%s&ie=UTF-8
+facebook,fb,https://www.facebook.com/search/top/?q=%s&ie=UTF-8&opensearch=1
+firefox-addons,fa,https://addons.mozilla.org/en-US/firefox/search/?q=%s&ie=UTF-8
+BangkokPost,bkp,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=site:bangkokpost.com+%s&ie=UTF-8
+Chocolatey,choco,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=site:chocolatey.org/packages+%s&ie=UTF-8
+Malwaretips,mal,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=site:malwaretips.com+%s&ie=UTF-8
+softpedia,s,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=site:softpedia.com+%s&ie=UTF-8
+faces,f,https://www.google.com/search?tbm=isch&q=%s&ie=UTF-8&ref=%opensearch&imgtype=face
+youtube,yt,https://www.youtube.com/results?search_query=%s&ie=UTF-8
+chromeextensions,e,https://www.google.com/search?gl=us&hl=en&safe=off&num=50&pws=0&q=site:chromewebstore.google.com+%s&ie=UTF-8
+G2A,g2a,https://www.g2a.com/search?query=%s&ie=UTF-8
+porna2,a2,https://www.google.com/search?hl=en&gl=us&safe=off&num=50&pws=0&udm=2&q=%s&ie=UTF-8 +porn|fuck|video|hd +-ladyboy+-shemale+-gay++intext:ass+intext:butt+intext:x+intext:anal
+edgeaddons,ea,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=site:microsoftedge.microsoft.com/addons/search+%s&ie=UTF-8
+porn,a,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&udm=2&q=%s&ie=UTF-8%20site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:eporner.com%20OR%20site:sxyprn.com%20OR%20site:thethothub.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+anal%20+-ladyboy+-shemale+-gay
+porn2,p,http://google.com/search?hl=en&safe=off&num=50&pws=0&q=%s&ie=UTF-8+anal+site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+%s&ie=UTF-8+-ladyboy+-shemale+-gay
+cloudsearch,c,https://cloudsearch.google.com/cloudsearch/search?authuser=0&q=%s&ie=UTF-8&it=1609926023312&iq=false
 `;
-
