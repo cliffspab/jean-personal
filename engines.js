652e932dc8106b4194714d84570bd877533827e1
cliffspab <75878433+cliffspab@users.noreply.github.com>
2025-10-09 05:27:00 +0700

    Update engines.js

--- engines.js
+++ engines.js
@@ -20,9 +20,9 @@ faces,f,https://www.google.com/search?tbm=isch&q=%s&ie=UTF-8&ref=%opensearch&img
 youtube,yt,https://www.youtube.com/results?search_query=%s&ie=UTF-8
 chromeextensions,e,https://www.google.com/search?gl=us&hl=en&safe=off&num=50&pws=0&q=site:chromewebstore.google.com+%s&ie=UTF-8
 G2A,g2a,https://www.g2a.com/search?query=%s&ie=UTF-8
-porna2,a2,https://www.google.com/search?hl=en&gl=us&safe=off&num=50&pws=0&udm=2&q=%s&ie=UTF-8 +porn|fuck|video|hd +-ladyboy+-shemale+-gay++intext:ass+intext:butt+intext:x+intext:anal
+porn2,a2,https://www.google.com/search?hl=en&gl=us&safe=off&num=50&pws=0&udm=2&q=%s +porn|fuck|video|hd +-ladyboy+-shemale+-gay++intext:ass+intext:butt+intext:x+intext:anal
 edgeaddons,ea,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&q=site:microsoftedge.microsoft.com/addons/search+%s&ie=UTF-8
 porn,a,https://www.google.com/search?hl=en&safe=off&num=50&pws=0&udm=2&q=%s&ie=UTF-8%20site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:eporner.com%20OR%20site:sxyprn.com%20OR%20site:thethothub.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+anal%20+-ladyboy+-shemale+-gay
-porn2,p,http://google.com/search?hl=en&safe=off&num=50&pws=0&q=%s&ie=UTF-8+anal+site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+%s&ie=UTF-8+-ladyboy+-shemale+-gay
+porn-old,p,https://google.com/search?hl=en&safe=off&num=50&pws=0&q=%s&ie=UTF-8+anal+site:pornhub.com%20OR%20site:xvideos.com%20OR%20site:4kporn.xxx%20OR%20site:sxyprn.com%20OR%20site:videosection.com%20OR%20site:spankbang.com%20OR%20site:bigporn.com%20OR%20site:xfantazy.com%20OR%20site:camflow.tv%20OR%20site:pornkai.com%20OR%20site:cambro.tv%20OR%20site:redtube.com%20OR%20site:txxx.com%20OR%20site:motherless.com+%s&ie=UTF-8+-ladyboy+-shemale+-gay
 cloudsearch,c,https://cloudsearch.google.com/cloudsearch/search?authuser=0&q=%s&ie=UTF-8&it=1609926023312&iq=false
 `;
