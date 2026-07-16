dac1f22e07ffee9ced508ee688a4f9e16c48e63e
cliffspab <75878433+cliffspab@users.noreply.github.com>
2026-07-16 12:43:34 +0700

    Refactor search engine URLs and remove obfuscation

--- engines.js
+++ engines.js
@@ -2,53 +2,25 @@
  * CSV-style list of custom search engines
  * LABEL,SHORTCUT,URL (with %s for search term)
  */
-
-// Core strict Google paths hidden in character-code sequences
-const _p1 = String.fromCharCode(104,116,116,112,115,58,47,47,119,119,119,46,103,111,111,103,108,101,46,99,111,109,47,115,101,97,114,99,104,63,113,61);
-const _p2 = String.fromCharCode(38,104,108,61,101,110,38,103,108,61,117,115,38,112,119,115,61,48,38,117,100,109,61,49,52,38,110,102,112,114,61,49,38,102,105,108,116,101,114,61,48,38,110,117,109,61,49,48,48,38,105,101,61,85,84,70,45,56,38,111,101,61,85,84,70,45,56,38,108,114,61,108,97,110,103,95,101,110);
-
-const _assemble = (domain) => {
-  const token = '%s';
-  return domain ? _p1 + 'site:' + domain + '+' + token + _p2 : _p1 + token + _p2;
-};
-
-// Rebuild non-Google systems out of safe fragments to block domain-stripping filters
-const _makeLink = (parts) => parts.join('');
-
-const _cracked    = _makeLink(['https', '://', 'cse.google.com', '/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&ie=UTF-8&gsc.sort=']);
-const _play       = _makeLink(['https', '://', 'play.google.com', '/store/search?q=%s&ie=UTF-8']);
-const _fb         = _makeLink(['https', '://', 'www.facebook.com', '/search/top/?q=%s&ie=UTF-8&opensearch=1']);
-const _firefox    = _makeLink(['https', '://', 'addons.mozilla.org', '/en-US/firefox/search/?q=%s&ie=UTF-8']);
-const _youtube    = _makeLink(['https', '://', 'www.youtube.com', '/results?search_query=%s&ie=UTF-8']);
-const _g2a        = _makeLink(['https', '://', 'www.g2a.com', '/search?query=%s&ie=UTF-8']);
-
-// Base obfuscated endpoints targeting Brave Search to bypass scrubbing proxies
-const _b1 = String.fromCharCode(104,116,116,112,115,58,47,47,115,101,97,114,99,104,46,98,114,97,118,101,46,99,111,109,47,115,101,97,114,99,104,63,113,61);
-const _b2 = String.fromCharCode(38,115,111,117,114,99,101,61,119,101,98);
-
-const _assembleBrave = () => {
-  return _b1 + '%s' + _b2;
-};
-
 window.engines = `
-Goog,g,${_assemble()}
-Cracked,zz,${_cracked}
-GitHub,gh,${_assemble('github.com')}
-Greasyfork,gf,${_assemble('greasyfork.org')}
-Reddit,r,${_assemble('reddit.com')}
-PlayStore,pl,${_play}
-brave,b,${_assembleBrave()}
-facebook,fb,${_fb}
-firefox-addons,fa,${_firefox}
-BangkokPost,bkp,${_assemble('bangkokpost.com')}
-Chocolatey,choco,${_assemble('chocolatey.org/packages')}
-Malwaretips,mal,${_assemble('malwaretips.com')}
-softpedia,s,${_assemble('softpedia.com')}
-faces,f,${_p1.replace('search?q=', 'search?tbm=isch&q=')}%s&ie=UTF-8&ref=%opensearch&imgtype=face
-youtube,yt,${_youtube}
-chromeextensions,e,${_assemble('chromewebstore.google.com')}
-G2A,g2a,${_g2a}
-edgeaddons,ea,${_assemble('microsoftedge.microsoft.com/addons')}
-cloudsearch,c,${_p1.replace('search?q=', 'cloudsearch/search?authuser=0&q=')}%s&ie=UTF-8&it=1609926023312&iq=false
+Goog,g,https://www.google.com/search?q=%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+Cracked,zz,https://cse.google.com/cse?cx=f8a43638082e49c2d#gsc.tab=0&gsc.q=%s&ie=UTF-8&gsc.sort=
+GitHub,gh,https://www.google.com/search?q=site:github.com+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+Greasyfork,gf,https://www.google.com/search?q=site:greasyfork.org+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+Reddit,r,https://www.google.com/search?q=site:reddit.com+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+PlayStore,pl,https://play.google.com/store/search?q=%s&ie=UTF-8
+brave,b,https://search.brave.com/search?q=%s&source=web
+facebook,fb,https://www.facebook.com/search/top/?q=%s&ie=UTF-8&opensearch=1
+firefox-addons,fa,https://addons.mozilla.org/en-US/firefox/search/?q=%s&ie=UTF-8
+BangkokPost,bkp,https://www.google.com/search?q=site:bangkokpost.com+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+Chocolatey,choco,https://www.google.com/search?q=site:chocolatey.org/packages+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+Malwaretips,mal,https://www.google.com/search?q=site:malwaretips.com+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+softpedia,s,https://www.google.com/search?q=site:softpedia.com+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+faces,f,https://www.google.com/search?tbm=isch&q=%s&hl=en&gl=us&ie=UTF-8&ref=%opensearch&imgtype=face&lr=lang_en
+youtube,yt,https://www.youtube.com/results?search_query=%s&hl=en&gl=us
+chromeextensions,e,https://www.google.com/search?q=site:chromewebstore.google.com+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+G2A,g2a,https://www.g2a.com/search?query=%s&ie=UTF-8
+edgeaddons,ea,https://www.google.com/search?q=site:microsoftedge.microsoft.com/addons/search+%s&hl=en&gl=us&pws=0&udm=14&nfpr=1&filter=0&num=100&ie=UTF-8&oe=UTF-8&lr=lang_en
+cloudsearch,c,https://cloudsearch.google.com/cloudsearch/search?authuser=0&q=%s&ie=UTF-8&it=1609926023312&iq=false
 jean,j,fuzzylogic.page/?q=%s
 `;
